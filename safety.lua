b5b3429daa20b092b2e9d2c76f3cf00c90c540f7
Beanzilla <david2bond2@gmail.com>
2022-01-01 15:02:02 -0500

    Update safety.lua

--- safety.lua
+++ safety.lua
@@ -17,7 +17,7 @@ minetest.register_node( ":technic:mineral_uranium", {
 if oreveins.GAMEMODE == "MTG" then
     local default = rawget(_G, "default") or nil
     -- Not radioactive, thus safer
-    minetest.register_node("oreveins:mineral_uranium", {
+    minetest.register_node("oreveins:uranium_ore", {
         description = oreveins.S("Uranium Ore"),
         tiles = { "oreveins_stone.png^oreveins_uranium.png" },
         is_ground_content = true,
